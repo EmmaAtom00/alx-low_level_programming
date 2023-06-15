4bb9c5298dc2d28fcfa75d2a44db248e2b02c9b4
Emmanuel <emmanuelmark501@gmail.com>
2023-06-15 12:10:56 -0700

    144 char

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	float num3;
 
 	printf("Size of a char: %zu byte(s)\n", sizeof(c));
-	printf("Size of a int: %zu byte(s)\n", sizeof(num));
+	printf("Size of an int: %zu byte(s)\n", sizeof(num));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(num1));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(num2));
 	printf("Size of a float: %zu byte(s)\n", sizeof(num3));
