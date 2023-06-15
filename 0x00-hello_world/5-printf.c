320382b527fdceaeb415a8ad74dc30cba45918c3
Emmanuel <emmanuelmark501@gmail.com>
2023-06-15 07:03:51 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,15 +2,14 @@
 
 /**
  * main - printing the string in printf function
- * 
  * Description: using the prinf function to print the statement
  * "with proper grammar, but the outcome is a piece of art"
  *
- * return: 0
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
