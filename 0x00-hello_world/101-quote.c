40d5eb0ea2b920b5a449fb749670a6ac8262db88
Emmanuel <emmanuelmark501@gmail.com>
2023-06-18 17:40:36 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program starts here (Entry level)
+ *
+ * Return: Always retirn 1 (success)
+ *
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
