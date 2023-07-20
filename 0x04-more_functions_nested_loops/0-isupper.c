5ed3fa64d4acbf2e51a663c4cc68563bc618f7b6
Emmanuel <emmanuelmark501@gmail.com>
2023-07-20 01:01:01 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include 'main.h'
+#include "main.h"
 
-/*
- * main - check if a letter/word is an uppercase
+/**
+ * _isupper - check if a letter/word is an uppercase
  *
  * @c: the letter/word to be checked
  *
@@ -12,14 +12,9 @@
 
 int _isupper(int c)
 {
-	if(c >= A && c <= Z)
+	if ((c >= 'A') && (c <= 'Z'))
 	{
-		_putchar('1');
-	}
-	
-	else if(c >= 0 && c <= 9)
-	{
-		_putchar('0');
+		return (1);
 	}
 
 	return (0);
