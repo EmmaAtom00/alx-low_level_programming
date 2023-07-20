d281ea695e0030a5d7fa18557c35fea6d97a7a80
Emmanuel <emmanuelmark501@gmail.com>
2023-07-20 01:40:39 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9 followed by a new line
+ *
+ * Return: return nothing
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n')
+}
