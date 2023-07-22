29dfd77d56446c62c73b0cf6cce0c631f5b99e25
Emmanuel <emmanuelmark501@gmail.com>
2023-07-22 02:18:37 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,30 +9,37 @@
 int main()
 {
 	int i;
+	int n;
 
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0)
 		{
+			n = i;
 			printf("fizz");
 			if(i != 101)
 				putchar(' ');
 		}
 		if (i % 5 == 0)
 		{
+			n = i;
 			printf("buzz");
 			if(i != 101)
 				putchar(' ');
 		}
 		if (i % 3 == 0 && i % 5 == 0)
 		{
+			n = i;
 			printf("fizzbuzz");
 			if(i != 101)
 				putchar(' ');
 		}
-		printf("%d", i);
-		if(i != 100)
-			putchar(' ');
+		if (i != n)
+		{
+			printf("%d", i);
+			if(i != 100)
+				putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
