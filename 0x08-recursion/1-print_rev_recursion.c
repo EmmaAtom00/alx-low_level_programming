22d8e1612b3db30f129e140f0735eeff2120eb17
Emmanuel <emmanuelmark501@gmail.com>
2023-08-02 06:02:30 -0700

    reverse a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print the reverse of a string
+ * @s: string to be reversed
+ * Return: return nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
