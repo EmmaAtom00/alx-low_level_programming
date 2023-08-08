68828ba171f3fd9ffb4d82f60c6471743239d17b
Emmanuel <emmanuelmark501@gmail.com>
2023-08-08 03:05:23 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers
+ * @argc: The argument count
+ * @argv: the argument vector
+ */
+
+int main(int argc, char **argv)
+{
+	int product;
+
+	if ( argv[1] == NULL)
+	{
+		printf("Error\n");
+	}
+	else if (argc == 3)
+	{
+		product = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", product);
+	}
+	else
+		printf("Error\n");
+	return (0);
+}
