9775a23eade7c976d975576f31262b4838521efb
Emmanuel <emmanuelmark501@gmail.com>
2023-08-14 06:05:17 -0700

    corrected betty error

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,3 @@
-/**
- * A header file that defines a macro named SIZE as an
- * abbreviation for the token 1024
- */
-
 #ifndef OBJECT_LIKE_MICRO
 #define OBJECT_LIKE_MICRO
 
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,8 +1,3 @@
-/**
- * A header file that defines a macro names PI
- * as an abbreviation for the token 3.14159265359
- */
-
 #ifndef PI_H
 #define PI_H
 
