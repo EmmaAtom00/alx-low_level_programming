2563cc4966d7e62bd049cef93381f02d5003c69c
Emmanuel <emmanuelmark501@gmail.com>
2023-08-15 07:44:44 -0700

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,15 @@
  * @owner: dogs owner
  */
 
-#include <stdlib.h>
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+#include <stdlib.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
