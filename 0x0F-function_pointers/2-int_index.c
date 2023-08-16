9953ebe9c8a36dd40e55277151cf972eeb09169c
Emmanuel <emmanuelmark501@gmail.com>
2023-08-16 08:43:20 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,15 +3,22 @@
 /**
  * int_index - function to search for an integer
  * @size: the number of elements in the array
- * @arrray: the array in which operation is to be peerformed
+ * @array: the array in which operation is to be peerformed
  * @cmp: a pointer to the function to be used to compare values
- * Return: return the index of the first element for which cmp does nit return 0
+ * Return: return the index of the first element
+ * for which cmp does nit return 0
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 	int findex;
+
+	if (!array || !size || !cmp)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
@@ -20,6 +27,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 			findex = i;
 			break;
 		}
+		else
+			findex = -1;
 	}
 
 	return (findex);
