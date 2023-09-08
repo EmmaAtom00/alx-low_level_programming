30758523b0892cf3b2afb919292216187eb14341
Emmanuel <emmanuelmark501@gmail.com>
2023-09-08 00:02:28 -0700

    using th write file descriptor functions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,33 +11,33 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	FILE *fd;
+	int fd;
 	ssize_t re, wr;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = fopen(filename, "r");
+	fd = open(filename, O_RDONLY);
 
-	if (fd == NULL)
+	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 
-	re = fread(buffer, sizeof(char), letters, fd);
+	re = read(fd, buffer, letters);
 	if (re == -1)
 	{
 		free(buffer);
-		fclose(fd);
+		close(fd);
 		return (0);
 	}
 	wr = write(STDOUT_FILENO, buffer, letters);
 	if (wr == -1)
 	{
 		free(buffer);
-		fclose(fd);
+		close(fd);
 		return (0);
 	}
-	fclose(fd);
+	close(fd);
 	return (re);
 }
