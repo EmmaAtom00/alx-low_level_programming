0e2336fe59e4d2524b7217d1fce2745e0de63988
Emmanuel <emmanuelmark501@gmail.com>
2023-09-08 00:41:55 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fp, no_strings = 0;
+	int fd, no_string = 0;
 	ssize_t wr;
-	
-	if (filename == NULL)
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		close(fd);
 		return (-1);
-
-	fp = open(filename, O_WRONLY | O_CREAT, 0600);
-	if (fp == -1)
-		return (-1);
-
-	while (*text_content != '\0')
-		no_strings++;
-
-	if (text_content)
-		wr = write(fp, text_content, no_strings);
-	if (wr == -1)
-		return (-1);
-
-	close(fp);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[no_string] != '\0')
+			no_string++;
+		wr = write(fd, text_content, no_string);
+		if (wr == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
 	return (1);
 }
